ceb2d3610cf3d48061574a9917d5568ea2eebc3e
Kaeix <rodrigoviclima@gmail.com>
2023-04-23 00:19:17 -0300

    23/04

--- static/script.js
+++ static/script.js
@@ -9,3 +9,16 @@ menuMobile.addEventListener("click", () => {
         : menuMobile.classList.replace("bi-x", "bi-list-nested")
         body.classList.toggle("menu-nav-active")
 });
+
+/* Fecha o menu quando clica e muda icone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list-nested")
+        }
+    })
+})
